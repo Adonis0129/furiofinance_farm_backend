d2c90eeff87aabfae7961cf912d79ea39736a011
AdonisGithub <95545280+AdonisGithub@users.noreply.github.com>
2023-01-17 00:58:22 +0900

    add pools

--- app/controllers/apys.js
+++ app/controllers/apys.js
@@ -7,8 +7,8 @@ const getFurFiBNBFarmApr = require("../helpers/getFurFIBNBFarmApr");
 const getRewardFromMint = require("../helpers/getRewardFromMint");
 const tokenPrices = require("../helpers/getTokenPrices");
 
-// const poolNames = ["DAI_BUSD", "USDC_BUSD", "USDC_USDT", "BUSD_USDT"];
-const poolNames = ["BUSD_USDT"];
+const poolNames = ["DAI_BUSD", "USDC_BUSD", "USDC_USDT", "BUSD_USDT"];
+// const poolNames = ["BUSD_USDT"];
 
 let apys = {};
 
