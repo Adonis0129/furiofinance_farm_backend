7071207e59991d1278f281ee83bb2bca63fc9465
Adonis <goldendores9826@gmail.com>
2023-02-08 13:09:50 +0900

    d

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.get('/api/computed-apys', apyController.getAPYS);
   
 setInterval(() => {
   apyController.calculateAndSave();
-}, 600000);
+}, 30000);
 
 
 // const server = http.createServer(app);
